c50b8077a82fb3e27cc7a0f7f3be35f9a78a481f
kenchaso <kenchasonakai@gmail.com>
2023-12-29 13:52:25 +0900

    add: role用のenumを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
+  enum role: { general: 0, admin: 1 }
 
   validates :nickname, presence: true, on: :account_setup
   validates :mattermost_id, presence: true, on: :account_setup
