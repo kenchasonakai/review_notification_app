9ab12e3eb2a2c808ff812a6b66ced505ade71216
kenchaso <kenchasonakai@gmail.com>
2023-12-30 15:01:12 +0900

    add: レビュー依頼用のviewを追加

---
+++ app/controllers/review_requests_controller.rb
@@ -0,0 +1,34 @@
+class ReviewRequestsController < ApplicationController
+  before_action :check_user_profile, only: %i[new create]
+
+  def index
+    @review_requests = ReviewRequest.includes(:reviewer, :reviewee).order(created_at: :desc)
+  end
+
+  def new
+    @review_request = current_user.review_requests.new
+  end
+
+  def create
+    @review_request = current_user.review_requests.new(review_request_params)
+    if @review_request.save
+      @review_request.notify
+      redirect_to review_requests_path, notice: 'レビュー依頼を作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_request_params
+    params.require(:review_request).permit(:url, :message)
+  end
+
+  def check_user_profile
+    return if current_user.profile_setup?
+
+    flash[:notice] = 'プロフィールを設定してください'
+    redirect_to edit_profile_path
+  end
+end
