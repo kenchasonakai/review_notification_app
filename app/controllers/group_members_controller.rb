135b2e3d66e6f7fbc3a4dd890a202e46f7eb6346
kenchaso <kenchasonakai@gmail.com>
2024-01-02 19:09:58 +0900

    add: グループメンバーの追加・削除機能の追加

---
+++ app/controllers/group_members_controller.rb
@@ -0,0 +1,15 @@
+class GroupMembersController < ApplicationController
+  def create
+    group = Group.find(params[:group_id])
+    user = User.find(params[:user_id])
+    group.users << user
+    redirect_to groups_path, notice: "#{user.nickname}を#{group.name}に追加しました。"
+  end
+
+  def destroy
+    group = Group.find(params[:group_id])
+    user = User.find(params[:id])
+    group.users.destroy(user)
+    redirect_to groups_path, notice: "#{user.nickname}を#{group.name}から外しました。"
+  end
+end
