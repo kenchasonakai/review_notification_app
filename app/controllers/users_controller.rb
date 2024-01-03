fc69a3831c133cfe110b95ec25785b3f3e9a7ff4
kenchaso <kenchasonakai@gmail.com>
2024-01-03 13:42:59 +0900

    add: ユーザー管理用のコントローラーを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    authorize @users
+  end
+
+  def show
+    @user = User.find_by(id: params["id"])
+    authorize @user
+  end
+
+  def edit
+    @user = User.find_by(id: params["id"])
+    authorize @user
+  end
+
+  def update
+    user = User.find_by(id: params["id"])
+    authorize user
+    user.update(user_params)
+    redirect_to users_path
+  end
+
+  def destroy
+    user = User.find_by(id: params["id"])
+    authorize user
+    user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :mattermost_id, :activated, :role)
+  end
+end
