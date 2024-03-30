dd228479b88aa7dd353dcfdf7f287ee563ecd38d
kenchaso <kenchasonakai@gmail.com>
2024-03-30 16:21:41 +0900

    add: 退会用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "oauth/callback" => "oauths#callback"
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
   delete "logout" => "user_sessions#destroy", :as => :logout
-  resource :profile, only: [:show, :edit, :update]
+  resource :profile, only: [:show, :edit, :update, :destroy]
   resources :review_requests, only: [:index, :new, :create]
   resources :groups do
     resources :group_members, only: [:create, :destroy], as: :members
